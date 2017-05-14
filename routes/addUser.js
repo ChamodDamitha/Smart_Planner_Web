31fb194f129c1f83c17dee8bed1c7b9b7fa63761
chamod <chamod.14@cse.mrt.ac.lk>
2017-05-14 19:37:24 +0530

    user model removed

--- routes/addUser.js
+++ routes/addUser.js
@@ -5,7 +5,6 @@ var express = require('express');
 var router = express.Router();
 
 
-var User=require('../models/user');
 
 router.post('/', function(req, res, next) {
 
--- routes/updateUser.js
+++ routes/updateUser.js
@@ -8,24 +8,23 @@ var express = require('express');
 var router = express.Router();
 
 
-var User=require('../models/user');
 
 router.get('/', function(req, res, next) {
     console.log(req.query.old_email);
     console.log(req.query.new_email);
     console.log(req.query.new_name);
-    User.findOneAndUpdate({email:req.query.old_email},{name:req.query.new_name , email:req.query.new_email},
-        function (err,user) {
-            if(err) {
-                res.send(err);
-            }
-            if(user==null){
-                res.send("No user exists by the given email...!");
-            }
-            else {
-                res.send("User was updated successfully...!");
-            }
-    });
+    // User.findOneAndUpdate({email:req.query.old_email},{name:req.query.new_name , email:req.query.new_email},
+    //     function (err,user) {
+    //         if(err) {
+    //             res.send(err);
+    //         }
+    //         if(user==null){
+    //             res.send("No user exists by the given email...!");
+    //         }
+    //         else {
+    //             res.send("User was updated successfully...!");
+    //         }
+    // });
 
 });
 
